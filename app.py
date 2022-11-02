b35267621ec648825347219ba0e5e4bb67ccd5c7
Guneet Singh <33905482+MrTall93@users.noreply.github.com>
2022-11-02 11:27:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/homepage')
-def helloIndex():
+@app.route('/')
+def home():
     return 'Hello New Day!'
-
-app.run(host='0.0.0.0', port=5000)
